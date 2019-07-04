bb3f1e0e2954386efda5bbcc8858b7b0582e3dcb
Dolav Nitay <dolavn16@gmail.com>
2019-07-04 17:50:44 +0300

    Added basic implementation of SGD

--- Main.py
+++ Main.py
@@ -1,31 +1,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from Optimizer import stochastic_gradient_descent
 
 
-def create_c_vecs(y):
+def get_mu(w, x, b):
+    max_label = max(y)+1
+    mus = [np.dot(x.T, w[i])+b[i] for i in range(max_label)]
+    mu = np.array([max([m[i] for m in mus]) for i in range(mus[0].shape[0])])
+    #mu = np.zeros(*mu.shape)
+    return mu
+
+
+def create_c_vecs(y, num_of_labels):
     ans = []
-    for val in range(max(y)+1):
+    for val in range(num_of_labels):
         ans.append([1 if elem == val else 0 for elem in y])
     return np.array(ans)
 
 
-def softmax_obj(x, y, w, b):
-    c = create_c_vecs(y)
-    max_label = max(y)+1
-    sum_all = sum([np.exp(np.dot(x.T, w[i])+b[i]) for i in range(max_label)])
-    val = -sum([np.dot(c[i].T, np.log(np.exp(np.dot(x.T, w[i])+b[i])/sum_all)) for i in range(max_label)])/len(x)
+def softmax_obj(x, y, num_of_labels, w, b):
+    c = create_c_vecs(y, num_of_labels)
+    mu = get_mu(w, x, b)
+    sum_all = sum([np.exp(np.dot(x.T, w[i])+b[i]-mu) for i in range(num_of_labels)])
+    val = -sum([np.dot(c[i].T, np.log(np.exp(np.dot(x.T, w[i])+b[i]-mu)/sum_all)) for i in range(num_of_labels)])/len(x)
     return val
 
 
-def softmax_grad(x, y, w, b):
-    c = create_c_vecs(y)
-    max_label = max(y)+1
-    sum_all = sum([np.exp(np.dot(x.T, w[i])+b[i]) for i in range(max_label)])
-    cis = [(np.exp(np.dot(x.T, w[i])+b[i])/sum_all)-c[i] for i in range(max_label)]
+def sp_exp(p):
+    ans = np.exp(p)
+    return np.exp(p)
+
+
+def softmax_grad(x, y, num_of_labels, w, b):
+    c = create_c_vecs(y, num_of_labels)
+    mu = get_mu(w, x, b)
+    sum_all = sum([sp_exp(np.dot(x.T, w[i])+b[i]-mu) for i in range(num_of_labels)])
+    cis = [(np.exp(np.dot(x.T, w[i])+b[i]-mu)/sum_all)-c[i] for i in range(num_of_labels)]
     cis = [1/len(x)*np.dot(x, elem) for elem in cis]
     cis = [elem.reshape(1, -1) for elem in cis]
     ans = np.concatenate(cis, axis=0)
-    bs = [np.dot(x.T, w[i]) + b[i] for i in range(max_label)]
+    bs = [np.exp(np.dot(x.T, w[i]) + b[i]-mu)/sum_all for i in range(num_of_labels)]
     bs = [np.dot(curr, np.ones(len(curr))) for curr in bs]
     bs = [curr - sum(c[i]) for i, curr in enumerate(bs)]
     bs = np.array([(1/len(x))*curr for curr in bs])
@@ -62,12 +76,42 @@ def gradient_test(f, g, dim):
     plt.savefig('gradient_test.png')
 
 
-x = np.array([[1, 2], [2, 2], [1, 0], [1, 1], [0, 2]]).T
-y = np.array([2, 3, 0, 1, 1])
+x = np.array([[5, 2], [2, 2], [1, 0], [1, 1], [0, 2], [1, 4], [2, 8], [1, 2], [2, 4], [1, 9]]).T
+y = np.array([2, 3, 0, 1, 1, 2, 3, 0, 3, 2])
 w = np.array([[0.2, 1], [0.1, 0], [1, 0], [0.1, 0]])
 b = np.array([1, 1, 1, 1])
-print(x)
-print(y)
+
+max_label = max(y)+1
+
+
+def obj(wb, indices):
+    num_of_labels = max(y)+1
+    x_batch = np.concatenate([x.T[i] for i in indices]).reshape(*x.T[0].shape, len(indices))
+    y_batch = np.array([y[i] for i in indices])
+    w_range = x.shape[0]*max_label
+    curr_w = wb[:w_range].reshape(max_label, x.shape[0])
+    curr_b = wb[w_range:]
+    return softmax_obj(x_batch, y_batch, num_of_labels, curr_w, curr_b)
+
+
+def grad(wb, indices):
+    num_of_labels = max(y)+1
+    x_batch = np.concatenate([x.T[i] for i in indices]).reshape(*x.T[0].shape, len(indices))
+    y_batch = np.array([y[i] for i in indices])
+    w_range = x.shape[0]*max_label
+    curr_w = wb[:w_range].reshape(max_label, x.shape[0])
+    curr_b = wb[w_range:]
+    wgrad, bgrad = softmax_grad(x_batch, y_batch, num_of_labels, curr_w, curr_b)
+    #print(wgrad)
+    #print(bgrad)
+    return np.concatenate((wgrad.flatten(), bgrad.T), axis=None)
+
+
+wb = np.concatenate((w.flatten(), b.T), axis=None)
+w, obj_train = stochastic_gradient_descent(len(x.T), max(y)+1, obj, grad, wb.shape, batch_size=2)
+plt.plot(range(len(obj_train)), obj_train)
+plt.show()
+exit()
 v = softmax_obj(x, y, w, b)
 print(v)
 vs = [v]
@@ -76,8 +120,8 @@ vs = [v]
 #              w.shape)
 for _ in range(100):
     g, gb = softmax_grad(x, y, w, b)
-    w = w - 0.1*g
-    b = b - 0.1*gb
+    w = w - 0.02*g
+    b = b - 0.02*gb
     vs.append(softmax_obj(x, y, w, b))
 v = softmax_obj(x, y, w, b)
 print(w, b)
---
+++ Optimizer.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+MAX_ITER = 40
+
+ALPHA_0 = 0.5
+BETA = 0.5
+C = 10e-4
+EPSILON = 10e-4
+
+
+def get_step_size(x, obj, grad, dir):
+    """
+    Chooses a value for the step size using Armijo's rule
+    :param x: A vector x
+    :param obj:  The objective function
+    :param grad:  The gradient of the objective function
+    :param dir: The search direction
+    :return: The value of the step size
+    """
+    alpha = ALPHA_0
+    for j in range(MAX_ITER):
+        phi = obj(x+alpha*dir)
+        if phi <= obj(x)+C*alpha*np.dot(grad, dir):
+            return alpha
+        else:
+            alpha = BETA*alpha
+    return 0
+
+
+def create_batches(data_size, batch_size):
+    num_of_batches = int(data_size/batch_size) + (1 if data_size % batch_size != 0 else 0)
+    curr_ind = 0
+    batches = []
+    indices = np.random.permutation(range(data_size))
+    for i in range(num_of_batches):
+        last_ind = curr_ind
+        curr_ind = min(curr_ind+batch_size, data_size)
+        batches.append(indices[last_ind: curr_ind])
+    print(batches)
+    return batches
+
+
+def stochastic_gradient_descent(data_size, num_of_labels, objective, gradient, input_shape,
+                                batch_size=1):
+    """
+    Performs a general one point iterative method on a given objective function.
+    :param objective: The objective function
+    :param gradient: The gradient of the objective function
+    :param input_shape: The shape of the input
+    :return: The optimal value of w, and the history of convergence on the train dataset,
+    and test dataset if given.
+    """
+    w = np.random.rand(*input_shape)
+    batches = create_batches(data_size, batch_size)
+    curr_batch_ind = 0
+    obj_train = []
+    r = range(MAX_ITER)
+    curr_batch = batches[curr_batch_ind]
+    grad1 = gradient(w, curr_batch)
+    for _ in r:
+        curr_batch = batches[curr_batch_ind]
+        curr_batch_ind = (curr_batch_ind+1) % len(batches)
+        g = gradient(w, curr_batch)
+        obj_train.append(objective(w, range(data_size)))
+        alpha = get_step_size(w, lambda x: objective(x, curr_batch), g, g)
+        alpha = 0.01
+        if alpha == 0:
+            break
+        w = w - alpha*g
+        if np.linalg.norm(g)/np.linalg.norm(grad1) <= EPSILON:
+            break
+    obj_train = [np.abs(obj - objective(w, range(data_size))) for obj in obj_train]
+    return w, obj_train
