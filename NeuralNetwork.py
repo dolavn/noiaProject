1206f5fc3960d410d1e4625af2ceb12326c18023
Dolav Nitay <dolavn16@gmail.com>
2019-07-07 16:30:50 +0300

    Started work on NeuralNetwork

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,20 +1,55 @@
 import numpy as np
 
 
-def relu_activation(x):
+def relu_activation_fp(x, W, b):
     return x
 
 
-def tanh_activation(x):
+def relu_activation_gp(x, W, b):
     return x
+
+
+def relu_activation_gx(x, W, b):
+    return x
+
+
+def tanh_activation_fp(x, W, b):
+    return x
+
+
+def tanh_activation_gp(x, W, b):
+    return x
+
+
+def tanh_activation_gx(x, W, b):
+    return x
+
+
+def sm_activation_fp(x, W, b):
+    return x
+
+
+def sm_activation_gp(x, W, b):
+    return x
+
+
+def sm_activation_gx(x, W, b):
+    return x
+
+
+RELU_ACTIVATION = (relu_activation_fp, relu_activation_gp, relu_activation_gx)
+TANH_ACTIVATION = (tanh_activation_fp, tanh_activation_gp, tanh_activation_gx)
+SOFTMAX_ACTIVATION = (sm_activation_fp, sm_activation_gp, sm_activation_gx)
+
+ACTIVATION_FUNCTIONS = [RELU_ACTIVATION, TANH_ACTIVATION, SOFTMAX_ACTIVATION]
 
 
 class Layer:
 
     def __init__(self, input_dim, output_dim, activation):
-        if activation not in [relu_activation, tanh_activation]:
+        if activation not in ACTIVATION_FUNCTIONS:
             raise BaseException("Invalid activation function")
-        self._activation = activation
+        self._forward_pass, self_gradient_params, self._gradient_x = activation
         self._input_dim = input_dim
         self._output_dim = output_dim
         self._weights = np.random.rand(input_dim, output_dim)
@@ -29,7 +64,7 @@ class Layer:
     def forward_pass(self, input):
         if self._input_dim != input.shape[0]:
             raise BaseException("Invalid dimensions")
-        return self._activation(np.dot(input, self._weights)+self._bias)
+        return self._forward_pass(input, self._weights, self._bias)
 
 
 class Network:
