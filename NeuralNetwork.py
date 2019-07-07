8924944db5994c4d09f8b30174f8f839549b4e41
Shlomit <shlomitelior@gmail.com>
2019-07-07 18:01:29 +0300

    adding 'fp' for two activations functions

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,11 +1,13 @@
 import numpy as np
+import numpy.matlib
 
 
 def relu_activation_fp(x, W, b):
-    return x
+    return np.maximum(np.dot(W, x)+(np.matlib.repmat(b, x.shape[1], 1)).T, 0)
 
 
 def relu_activation_gp(x, W, b):
+    dg = np.append(W, np.ones(W.shape[1]))
     return x
 
 
@@ -14,7 +16,8 @@ def relu_activation_gx(x, W, b):
 
 
 def tanh_activation_fp(x, W, b):
-    return x
+    return np.tanh(np.dot(W, x)+(np.matlib.repmat(b, x.shape[1], 1)).T)
+
 
 
 def tanh_activation_gp(x, W, b):
@@ -89,5 +92,11 @@ class Network:
 
 
 if __name__ == '__main__':
-    l = Layer(2, 5, relu_activation)
+    #l = Layer(2, 5, relu_activation)
     a = np.array([1, 2, 1])
+    x = np.array([[-5, -2], [2, -2], [-1, 0], [-1, 1], [0, 2], [1, 4], [2, 8], [1, 2], [2, 4], [1, 9]]).T
+    #y = np.array([2, 3, 0, 1, 1, 2, 3, 0, 3, 2])
+    W = np.array([[0.2, 1], [0.1, 0], [1, 0], [0.1, 0]])
+    b = np.array([0, 1, 0, 1])
+    dg = np.append(W, np.ones(W.shape[1]))
+    print(dg)
